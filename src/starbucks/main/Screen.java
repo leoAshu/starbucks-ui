d5ba6e5fc4dad17185b76512b0634d97099f345a
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-25 14:34:52 -0700

    creates base class for screen following composite pattern

---
+++ src/starbucks/main/Screen.java
@@ -0,0 +1,40 @@
+import java.util.List;
+import java.util.ArrayList;
+
+/**
+ * Base Class for Screens.
+ * 
+ * Provides Common Functionality
+ * For Setting Up the Composite and 
+ * Chain of Responsibility Patterns.
+ * 
+ */
+public class Screen implements IScreen, IDisplayComponent {
+    private List<IDisplayComponent> components;
+
+    public Screen() {
+        this.components = new ArrayList<IDisplayComponent>();
+    }
+
+    @Override
+    public void touch(int x, int y) {
+        
+    }
+
+    @Override
+    public void release() {
+
+    }
+
+    @Override
+    public void display() {
+       for(IDisplayComponent component: components)
+            component.display();
+    }
+
+    @Override
+    public void addSubComponent(IDisplayComponent component) {
+        components.add(component);
+    }
+    
+}
