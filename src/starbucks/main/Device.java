d376e1b874ac2d7048b67a8fa70bfc81ae6a0419
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-25 14:50:38 -0700

    configures and displays pin screen on device

--- src/starbucks/main/Device.java
+++ src/starbucks/main/Device.java
@@ -5,13 +5,17 @@ import processing.core.PImage;
  * Authentication Proxy for App Controller
  */
 public class Device implements IApp  {
-    private static Device theDevice;
     private PApplet starbucks;
+    private static Device theDevice;
+
+    private boolean authenticated;
     private PImage notificationBar;
+    private PinScreen pinScreen;
 
     private Device(PApplet starbucks) {
         this.starbucks = starbucks;
         this.notificationBar = starbucks.loadImage(Constants.NOTIF_BAR_IMAGE_PATH);
+        startUp();
     }
 
     public synchronized static Device getDevice(PApplet starbucks) {
@@ -24,6 +28,22 @@ public class Device implements IApp  {
         theDevice = new Device(starbucks);
         return theDevice;
     }
+
+    /**
+     * Device Starup Process.  
+     * Starts Up with Default 4-Pin Option
+     */
+    private void startUp() {
+        setUpPinScreen();
+    }
+
+    /**  
+     * Sets up the Pin Screen
+     */
+    private void setUpPinScreen() {
+        pinScreen = new PinScreen(starbucks);
+    }
+
 
     @Override
     public void touch(int x, int y) {
@@ -44,6 +64,14 @@ public class Device implements IApp  {
             Constants.NOTIF_BAR_WIDTH,
             Constants.NOTIF_BAR_HEIGHT
         );
+        screenDisplay();
+    }
+
+    private void screenDisplay() {
+        if(authenticated) {
+            // App Screen
+        } else 
+            pinScreen.display();
     }
     
 }
