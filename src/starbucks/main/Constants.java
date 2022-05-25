d7adb2d6c947629cabb74b942faebac1e7ac5653
leoAshu <ashutosh.ojha2009@gmail.com>
2022-05-25 13:56:16 -0700

    adds constant values for notification bar

--- src/starbucks/main/Constants.java
+++ src/starbucks/main/Constants.java
@@ -1,4 +1,10 @@
 public class Constants {
+    // screen dimensions
     public static final int SCREEN_WIDTH = 375;
     public static final int SCREEN_HEIGHT = 593;
+
+    // notification bar
+    public static final String NOTIF_BAR_IMAGE_PATH = "../../assets/images/notification-bar-1.5x.png";
+    public static final int NOTIF_BAR_HEIGHT = 31;
+    public static final int NOTIF_BAR_WIDTH = 375;
 }
